cbf176471c8b064f144ee7cfcb5370990fa1fe47
scarey3-wisc <55416386+scarey3-wisc@users.noreply.github.com>
2020-06-13 10:44:52 -0600

    Adding comments

--- js/actionitem.js
+++ js/actionitem.js
@@ -1,8 +1,12 @@
+//An ActionItem is a selectable option in a menu that, when clicked, either immediately does the action or prompts the user for
+//more details about the action (ie, clicking a move option will highlight all the spaces you can move)
 function ActionItem(unit, actionName, controller, scenario){
 	this.unit = unit;
 	this.actionName = actionName;
 	this.menuString = this.actionName;
 	this.controller = controller;
+	//scenario is an object with a few "special functions" if the action is unique to the current battle and requires special
+	//handling (ie, a gladiator fight may have a "cheer the crowd" option)
 	if(scenario)
 		this.setupScenario(scenario);
 	else
@@ -13,6 +17,8 @@ ActionItem.prototype.setupScenario = function(scenario){
 	this.whenSelected = scenario.whenSelected;
 }
 ActionItem.prototype.discoverType = function(){
+	//a bunch of actions with different names, like different attacks or movement options, are essentially the same; this categorizes
+	//those actions so that they can all be handled by the same code.
 	if(this.actionName == "Walk")
 		this.setupType("move");
 	if(this.actionName == "Dash")
@@ -51,6 +57,7 @@ ActionItem.prototype.setupType = function(type){
 	if(type == "move"){
 		this.doable = this.moveDoable();
 		this.whenSelected = this.whenMoveSelected;
+		//we want to specify in the visible menu how much movement the character actually has
 		if(Math.floor(this.unit.stride) == 1)
 			this.menuString = this.actionName + " (" + Math.floor(this.unit.stride) + " space)";
 		else
@@ -66,9 +73,11 @@ ActionItem.prototype.setupType = function(type){
 		this.doable = this.selfDoable();
 		this.whenSelected = this.getWhenSelfSelected();
 	}else if(type == "ranged"){
+		//we might want to do ranged attacks in the future!
 		this.doable = false;
 		this.whenSelected = function(){};
 	}else if(type == "spell"){
+		//we might want to do spell attacks in the future!
 		this.doable = false;
 		this.whenSelected = function(){};
 	}else{
@@ -86,6 +95,7 @@ ActionItem.prototype.meleeDoable = function(){
 	this.spaceList = [];
 	var unit = this.unit;
 	var map = unit.map;
+	//is there anybody nearby that we could actually hit with a sword?
 	if(map.getContent({x:unit.x, y:unit.y+1}) && unit.canTravel(map.getEdge(unit.x, unit.y, 'd')))
 		this.spaceList.push(map.getContent({x:unit.x, y:unit.y+1}));
 	if(map.getContent({x:unit.x+1, y:unit.y}) && unit.canTravel(map.getEdge(unit.x, unit.y, 'r')))
